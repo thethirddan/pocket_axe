94702ac0aefd1837c4c6ad763666ae09e43f02f5
Dan Baumfeld <thethirddan@gmail.com>
2015-09-14 12:44:26 -0700

    components

---
+++ components/user.js
@@ -0,0 +1,18 @@
+@User = React.createClass({
+
+        propTypes: {},
+        mixins : [],
+
+        getInitialState: function() {},
+        getDefaultProps: function() {},
+
+        componentWillMount : function() {},
+        componentWillReceiveProps: function() {},
+        componentWillUnmount : function() {},
+
+        _parseData : function() {},
+        _onSelect : function() {},
+
+        render : function() {}
+
+    })
